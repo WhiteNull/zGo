60095502b62a4a403adb29a6da07bc2b203438d8
GongZh <237472448@qq.com>
2019-02-10 14:20:38 +0800

    change ev

---
+++ ChangeToAlpha.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	b, err := ioutil.ReadFile("/etc/woda/GlobalConfigure_alpha.yaml")
+	if err != nil {
+		fmt.Print(err)
+	}
+	err = ioutil.WriteFile("/etc/woda/GlobalConfigure.yaml", b, 0644)
+	if err != nil {
+		fmt.Print(err)
+	} else {
+		fmt.Print("change to alpha success")
+	}
+}

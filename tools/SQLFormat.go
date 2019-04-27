022175c5f74a27537194688c6488f249f8b58bc2
GongZh <237472448@qq.com>
2019-04-27 13:51:55 +0800

    add date value err check

--- tools/SQLFormat.go
+++ tools/SQLFormat.go
@@ -3,20 +3,45 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
 func replaceValue(strsql string) (string, error) {
 	s := strings.Index(strsql, "[")
 	e := strings.LastIndex(strsql, "]")
 	if s <= 0 || e <= 0 {
-		return "", fmt.Errorf("要格式化的SQL不正确")
+		return "", fmt.Errorf("未找到[]中的值")
 	}
 	sql := strsql[:s]
 	sqlvalue := strsql[s+1 : e]
 	values := strings.Split(sqlvalue, " ")
-	for i := 0; i < len(values); i++ {
-		value := values[i]
-		sql = strings.Replace(sql, "?", value, 1)
+	//判断sql语句中?的数量
+	countq := strings.Count(sql, "?")
+	if countq == len(values) {
+		for i := 0; i < len(values); i++ {
+			value := values[i]
+			sql = strings.Replace(sql, "?", value, 1)
+		}
+	} else if len(values) > countq {
+		//一个时间中可能含有空格导致分割值出错
+		for i := 0; i < len(values); i++ {
+			value := values[i]
+			if i < len(values)-1 {
+				nextvalue := values[i+1]
+				timev := value + " " + nextvalue
+				_, err := time.Parse("2006-01-02 15:04:05", timev)
+				if err != nil {
+					sql = strings.Replace(sql, "?", value, 1)
+				} else {
+					sql = strings.Replace(sql, "?", timev, 1)
+					i++
+				}
+			} else {
+				sql = strings.Replace(sql, "?", value, 1)
+			}
+		}
+	} else {
+		return "", fmt.Errorf("[]中的值数量比sql中的?数量少")
 	}
 	return sql, nil
 }
@@ -25,7 +50,7 @@ func replaceValue(strsql string) (string, error) {
 func main() {
 	fmt.Println("sql format start")
 
-	strsql := "UPDATE `user_clock_rec` SET `advance_pay_amt` = ?, `calculate_pay_sts` = ?, `clock_in_is_valid` = ?, `clock_in_sts` = ?, `clock_in_tm` = ?, `clock_in_typ` = ?, `clock_out_is_valid` = ?, `clock_out_sts` = ?, `clock_out_tm` = ?, `clock_out_typ` = ?, `clock_sts` = ?, `name_list_id` = ?, `remark` = ?, `updated_tm` = ?, `uuid` = ?  WHERE (uuid = ? OR user_id = ?) AND (clock_dt = ?) AND (is_deleted = ?) [0 0 1 1 2019-04-13 19:51:33 2 1 1 2019-04-14 07:52:39 2 1 127785  2019-04-15 14:46:10 151746 151746 154921 2019-04-13 1]"
+	strsql := "UPDATE `user_clock_rec` SET `advance_pay_amt` = ?, `calculate_pay_sts` = ?, `clock_out_addr` = ?, `clock_out_device` = ?, `clock_out_ent_loc_id` = ?, `clock_out_is_valid` = ?, `clock_out_latitude` = ?, `clock_out_longitude` = ?, `clock_out_sts` = ?, `clock_out_tm` = ?, `clock_out_typ` = ?, `clock_pay_amt` = ?, `clock_sts` = ?, `credit_subsidy_amt` = ?, `name_list_id` = ?, `uuid` = ?  WHERE `user_clock_rec`.`user_clock_rec_id` = ? [2231 5 222222222 设备Id 0 1 19211 19211 2 2019-04-27 10:51:21 1 2232 2 2233 16223 251722 100000120]"
 
 	esql, err := replaceValue(strsql)
 	if err != nil {
